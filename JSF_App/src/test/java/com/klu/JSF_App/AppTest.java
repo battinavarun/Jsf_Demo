c8d5bbbcbccb8b1413509fc3dede8cc1e85921bf
BATTINA VARUN <119784155+battinavarun@users.noreply.github.com>
2024-10-19 16:18:14 +0530

    Add files via upload

---
+++ JSF_App/src/test/java/com/klu/JSF_App/AppTest.java
@@ -0,0 +1,20 @@
+package com.klu.JSF_App;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
